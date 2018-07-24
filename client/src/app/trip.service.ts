41fb062c60209b4ea07b49e226ef976bc961848d
Gustavo Monardez <gustavomonardez@hotmail.com>
2018-07-24 16:28:46 -0700

    added drag and drop for suggestions and proposed

--- client/src/app/trip.service.ts
+++ client/src/app/trip.service.ts
@@ -8,13 +8,14 @@ import { HttpClient } from '@angular/common/http';
 export class TripService {
   apiUrl = "http://localhost:8080/api/";
   constructor(private _http:HttpClient) { }
-
+  /********************USERS********************/
   registerUser(user:{}){
     return this._http.post(this.apiUrl+"users",user);
   }
   loginUser(user:{}){
     return this._http.post(this.apiUrl+"loginuser",user);
   }
+  /********************TRIPS********************/
   createTrip(trip:{}, userId:string){
     var tripPost = {
       "trip":trip,
@@ -22,5 +23,25 @@ export class TripService {
     }
     return this._http.post(this.apiUrl+"trips",tripPost);
   }
-
+  findTripById(trip_id){
+    return this._http.get(this.apiUrl+"trips/"+trip_id);
+  }
+  /********************ACTIVITIES********************/
+  findAllActivities(){
+    return this._http.get(this.apiUrl+"activities");
+  }
+  createActivity(activity:{}){
+    return this._http.post(this.apiUrl+"activities",activity);
+  }
+  addActivityToAgenda(activity:{},agenda_id){
+    ///agendas/{agenda_id}/edit
+    return this._http.post(this.apiUrl+"agendas/"+agenda_id+"/edit",activity);
+  }
+  /********************AGENDA********************/
+  createAgenda(agenda:{}){
+    return this._http.post(this.apiUrl+"agendas",agenda); 
+  }
+  findAgendaById(agenda_id){
+    return this._http.get(this.apiUrl+"agendas/"+agenda_id);
+  }
 }
